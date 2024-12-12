128412572d5aa95f528bb8a214903d52dfab09ec
kitano <beiyelinai01@gmail.com>
2024-12-12 13:31:04 +0900

    更新

--- src/resources/views/products.blade.php
+++ src/resources/views/products.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('css')
-<link rel="stylesheet" href="{{ asset('products.css') }}">
+<link rel="stylesheet" href="{{ asset('css/products.css') }}">
 @endsection
 
 @section('content')
@@ -11,24 +11,29 @@
             <h2>商品一覧</h2>
         </div>
         <div class="header__button">
-            <button type="submit" class="header__button-submit">・商品を追加</button>
+            <button type="submit" class="header__button-submit">＋ 商品を追加</button>
         </div>
     </div>
-    <div class="search-form">
-        <div class="search-form-name">
-            <input type="text" name="" value="" class="search-form__input-name">
+    <div class="list__content">
+        <div class="search-form">
+            <div class="search-form-name">
+                <input type="search" name="" id="" class="search-form__input-name" placeholder="商品名で検索">
+            </div>
+            <div class="search-form__button-name">
+                <button type="submit" class="search-form__button-submit-name">検索</button>
+            </div>
+            <div class="search-form__title">
+                <p class="search-form__title-value">価格順で表示</p>
+            </div>
+            <div class="search-form__input">
+                <input type="search" name="" id="" class="search-form__input-value">
+            </div>
         </div>
-        <div class="search-form__button-name">
-            <button type="submit" class="search-form__button-submit-name">検索</button>
+        <div class="list-image">
+            <a href="" class="list-mage__item">
+                <img src="../../storage/fruits-img/banana.png" alt="banana">
+            </a>
         </div>
-        <div class="search-form-value">
-            <input type="search" name="" id="" class="search-form__input-value">
-        </div>
-    </div>
-    <div class="list-image">
-        <a href="" class="list-mage__item">
-            <img src="../../storage/fruits-img/banana.png" alt="banana">
-        </a>
     </div>
 </div>
 @endsection
