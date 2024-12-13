8bd2931fc9550a3360311a53ee4899e40fbe21ba
kitano <beiyelinai01@gmail.com>
2024-12-13 10:21:09 +0900

    更新

--- src/database/migrations/2024_12_12_152914_create_seasons_table.php
+++ src/database/migrations/2024_12_12_152914_create_seasons_table.php
@@ -15,7 +15,7 @@ class CreateSeasonsTable extends Migration
     {
         Schema::create('seasons', function (Blueprint $table) {
             $table->id();
-            $table->string('name',255);
+            $table->string('name',255)->unique();
             $table->timestamps();
         });
     }
